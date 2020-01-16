7b737b7ae1120e83961013d178fb0539af067c61
Jessica Bailey <JessBailey@Jessicas-MacBook-Air.local>
2020-01-16 16:50:10 +0000

    all css

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import LogIn from "./LogIn";
 import SignUp from "./SignUp";
 import "./Home.css";
+import logo from "../assets/fomo-logo.png";
 
 const Home = ({ getUser }) => {
   return (
     <div className="HomepageContainer">
-      <div className="LogoContainer"></div>
+      <div className="LogoContainer"><img src={logo}></img></div>
       <div className="HomepageFormContainer">
         <LogIn getUser={getUser} />
         <SignUp />
